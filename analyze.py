c880f98af1cabdb0d62bef598a0fff85dba0eb1d
mahdafr <mmafravi@gmail.com>
2024-11-17 23:08:58 -0500

    v0.2 released: added multiple dropdown selection (status), updated return string for structured output

--- analyze.py
+++ analyze.py
@@ -5,9 +5,15 @@ import pandas as pd
 def _read(f_path):
     return pd.read_csv(f_path, header=0)
 
-def _to_string(df):
-    return "There are {0} characters to print including the header row.\n{1}".format(len(df.to_string()),
-                                                                                                                       df.to_string())
+def _to_string(df, date):
+    return "Unique Company Names:\n{3}\n\
+                \nUnique Role Titles:\n{2}\n\
+                \nEntries:\n{0}\n\
+                \nCharacters (incl. header row): {1}".format(
+                                    df.to_string(index=False), # print the table w/out row #s
+                                    len(df.to_string(index=False)), # total char count w/out row #s
+                                    df.role.unique(), # unique roles
+                                    df['company name'].unique()) # unique companies
 
 def _filter_by(df_all, date, status):
     df_date = df_all.loc[df_all['date'] == date]
@@ -21,11 +27,15 @@ def _error_check(f_path, date, status):
     if status is None:
         raise gr.Error("No status selected. Please try again by selecting an application status to filter the data.")
 
-def analyze(f_path, date, status):
-    _error_check(f_path, date, status)
+def analyze(f_path, date, status_list):
+    date = str(date.date())
+    _error_check(f_path, date, status_list)
     df_all = _read(f_path)
-    df = _filter_by(df_all, date, status)
-    return _to_string(df)
+    ret_str = "Date: {0}\n\n".format(date)
+    for status in status_list:
+        df = _filter_by(df_all, date, status)
+        ret_str += "============{0}============\n\n{1}\n\n".format(status.upper(), _to_string(df, date))
+    return ret_str
 
 
 options = [
@@ -33,11 +43,12 @@ options = [
     ("Applying", "applying"),
     ("Applied", "applied"),
     ("Interviewing", "interviewing"),
+    ("Rejected", "not_selected"),
 ]
 
 ui_elem = [gr.FileExplorer(label="CSV File", glob="*.csv", file_count="single"),
-                gr.DateTime(label="Date to Examine", info="Filter the data to view only rows with this selected date", include_time=False),
-                gr.Dropdown(label="Application Status", info="Filter the data to view only this selected application status", choices=options)]
+                gr.DateTime(label="Date to Examine", info="Filter the data to view only rows with this selected date", include_time=False, type="datetime"),
+                gr.Dropdown(label="Application Status", multiselect=True, info="Filter the data to view only the selected application statuses", choices=options)]
 
 demo = gr.Interface(
                     title="TealHQ Data Parsing",
@@ -48,6 +59,8 @@ demo = gr.Interface(
                     allow_flagging="never",
                     clear_btn=gr.ClearButton(components=ui_elem, value="Clear form"),
                     submit_btn=gr.Button(value="Filter", variant="primary"),
-                    theme=gr.themes.Soft())
+                    theme=gr.themes.Soft()
+            )
+
 
 demo.launch()
